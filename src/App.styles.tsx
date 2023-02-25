10be16fa7c78da4b846e4dff968e1bfb06a21d43
Kubix007 <kuba.dolinski@hotmail.com>
2023-02-25 10:14:39 +0100

    Update background image

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "@mui/material";
 import styled from "styled-components";
+import backgroundImage from "./img/backgroundImage.jpg";
 
 export const Center = styled(Stack)`
   height: 100vh;
@@ -7,6 +8,6 @@ export const Center = styled(Stack)`
 `;
 
 export const App = styled.div`
-  background: url(https://pixabay.com/get/g77ecd3d4bd995d304427d6c503adeede405ea3d422758dd22b9819e42bc258e769777c0b127e587c8c4490f72e8a87e7f5ebb2d3bd03b852c4881b7b96392974_1920.jpg);
+  background: url(${backgroundImage});
   background-size: cover;
 `;
